1bcdf485d81e130cc5622c0945c62559d288a391
favour-samson <samsonfavour122000@gmail.com>
2024-08-16 10:10:38 +0100

    first commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am{" "}
+            <span className="purple">Thomas Samson Favour </span>
+            from <span className="purple"> Lagos, Nigeria.</span>
+            <br />
+            I am currently a full-stack developer <br />
+            I studied Remote Sensing and GIS and graduated from the Federal
+            University of Technology, Akure.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Watching movies
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Favour</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/project1.png";
+import editor from "../../Assets/Projects/project2.png";
+import chatify from "../../Assets/Projects/project3.png";
+import suicide from "../../Assets/Projects/project4.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="HookBank"
+              description="Hoobank revolutionizes how you manage credit cards. Our expert team uses a cutting-edge methodology to identify the cards that best suit your financial needs. We analyze factors such as annual percentage rates and fees to help you make informed decisions and maximize your financial potential."
+              ghLink="https://github.com/favour-samson/hook-bank"
+              demoLink="https://hookbank-ui.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Table Cloth"
+              description="Tablecloth Finance is your hub for money matters, news, content, and tools. Whether you’re working on becoming debt free, buying a house, exploring investment opportunities, or planning for retirement, we’ll guide you along the path to financial freedom."
+              ghLink="https://github.com/favour-samson/tablecloth"
+              demoLink="https://tablecloth-finance-ui.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="Furnishop"
+              description="Furnitre power is a software as services for multiperpose business management system, expecially for them who are running two or more business exploree the future Furnitre power is a software as services."
+              ghLink="https://github.com/favour-samson/furnishop"
+              demoLink="https://furnishop-lilac.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title="BGL"
+              description="We are one of Africa's leading Securities Trading and Dealing Companies headquartered in Lagos, Nigeria. We provide world dealing services and products to clients BGL Securities leverages on the distinctive BGL brand, harnessing its core competence and unique human resources to deliver excellent and value added services to clients"
+              ghLink="https://github.com/favour-samson/stockapp"
+              demoLink="https://stockapp-ashy.vercel.app/"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
